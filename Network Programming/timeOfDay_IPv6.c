8f70598f4b58b7a06b4e8c31255cef9eec825645
Vignesh Vedavyasan <tovigu@gmail.com>
2019-04-26 10:49:38 +0530

    Renaming timeOfDat_IPv6.c to timeOfDay_IPv6.c =============================================
    Changed err_sys and err_quit to perror and printf

--- Network Programming/timeOfDay_IPv6.c
+++ Network Programming/timeOfDay_IPv6.c
@@ -9,29 +9,29 @@ main(int argc, char **argv) {
     struct sockaddr_in6 servaddr;
 
     if(argc != 2)
-        err_quit("Usage : timenow_IPv6 <IP-address>");
+        printf("Usage : timenow_IPv6 <IP-address>");
 
     if((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
-        err_sys("Socket error");
+        perror("Socket error");
 
     bzero(&servaddr, sizeof(servaddr));
-    servaddr.family = AF_INET6;
-    servaddr.port = htons(13);
+    servaddr.sin6_family = AF_INET6;
+    servaddr.sin6_port = htons(13);
 
     if(inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) <= 0)
-        err_quit("inet_pton error for %s\n", argv[1]);
+        printf("inet_pton error for %s\n", argv[1]);
 
     if(connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
-        err_sys("connect error");
+        perror("connect error");
 
     while((n = read(sockfd, &recvLine, MAXLINE)) > 0) {
         recvLine[n] = 0;
         if(fputs(recvLine,stdout) == EOF)
-            err_sys("fputs error");
+            perror("fputs error");
     }
 
     if(n < 0)
-        err_sys("read error");
+        perror("read error");
 
     exit(0);
 }

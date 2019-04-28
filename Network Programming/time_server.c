5917bc35a2535a1f211d22991a2d169db77ac050
Vignesh Vedavyasan <tovigu@gmail.com>
2019-04-28 19:02:39 +0530

    Comments to server code

--- Network Programming/time_server.c
+++ Network Programming/time_server.c
@@ -15,16 +15,33 @@ main(int argc, char **argv) {
     
     memset(&servaddr, '\0', sizeof(servaddr));
     servaddr.sin_family = AF_INET;
+    /* sin_addr.s_addr should accept connection from any interface */
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(13);
 
     if((bind(listenfd, (SA *)&servaddr, sizeof(servaddr))) < 0)
         perror("bind error");
 
+    /* 
+     * listen (int,int)
+     * Marks a connection-mode socket as accepting connection and
+     * limits the number of outstanding connections in the socket's
+     * listen queue to the macro LISTENQ
+     */
+
     if((listen(listenfd, LISTENQ)) < 0)
         perror("listen error");
 
     for(;;) {
+        /* 
+         * Normally server is put to sleep in the call to accept,
+         * waiting for the client connection to arrive and be
+         * accepted. TCP connection uses a three-way handshake
+         * to establish the connection. When handshake succeeds
+         * accept returns value which is the connection descriptor.
+         * The connection descriptor thus obtained is used for
+         * communication with the client.
+         */
         if((connfd = accept(listenfd, (SA *) NULL, NULL)) < 0)
             perror("accept error");
 
